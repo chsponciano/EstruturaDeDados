75d0704b65418b69646cb44d27d695d73186be79
Carlos <chs.ponciano@gmail.com>
2018-07-03 13:23:49 -0300

    Correção MergeSort

--- Ordenacao - L12/src/OrdenacaoMergeSort.java
+++ Ordenacao - L12/src/OrdenacaoMergeSort.java
@@ -23,65 +23,29 @@ public class OrdenacaoMergeSort<T extends Comparable<T>> extends OrdenacaoAbstra
 
     //Intercalar
     private void merge(int inicio, int fim, int meio) {
-//        Professor
-        int tmEsq = meio - inicio + 1;
-        T[] esquerda = (T[]) new Comparable[tmEsq];
-        for(int i = 0; i < tmEsq; i++){
-            esquerda[i] = this.getInfo()[i];
+        T[] data = super.getInfo();
+        T[] temp = (T[]) new Comparable[data.length];
+
+        for (int i = 0; i < temp.length; i++) {
+            temp[i] = data[i];
         }
-        
-        int tmDir = fim - meio;
-        T[] direita = (T[]) new Comparable[tmDir];
-        for(int i = 0; i < tmDir; i++){
-            direita[i] = this.getInfo()[meio+1+i];
-        }
-        
-        int cEsq = 0, cDir = 0;
-        int i = 0;
-        for(i = inicio; i <= fim; i++){
-            if((cEsq < tmEsq) && (cDir < tmDir)){
-                if(esquerda[cEsq].compareTo(direita[cDir]) < 0){
-                    this.getInfo()[i] = esquerda[cEsq++];
-                }else{
-                    this.getInfo()[i] = direita[cDir++];
-                }
-            }else{
-                break;
+
+        int auxInicio = inicio;
+        int auxMeio = meio + 1;
+
+        for (int i = inicio; i <= fim; i++) {
+            if (auxInicio == meio + 1) {
+                data[i] = temp[auxMeio++];
+            } else if (auxMeio > fim) {
+                data[i] = temp[auxInicio++];
+            } else if (temp[auxInicio].compareTo(temp[auxMeio]) <= 0) {
+                data[i] = temp[auxInicio++];
+            } else {
+                data[i] = temp[auxMeio++];
             }
         }
-        
-        while(cEsq < tmEsq){
-            this.getInfo()[i++] = esquerda[cEsq++];
-        }
-        while(cDir < tmDir){
-            this.getInfo()[i++] = direita[cDir++];
-        }
-        
-        
-//        Codigo antigo
-//        T[] data = super.getInfo();
-//        T[] temp = (T[]) new Comparable[data.length];
-//
-//        for (int i = 0; i < temp.length; i++) {
-//            temp[i] = data[i];
-//        }
-//
-//        int auxInicio = inicio;
-//        int auxMeio = meio + 1;
-//
-//        for (int i = inicio; i <= fim; i++) {
-//            if (auxInicio == meio + 1) {
-//                data[i] = temp[auxMeio++];
-//            } else if (auxMeio > fim) {
-//                data[i] = temp[auxInicio++];
-//            } else if (temp[auxInicio].compareTo(temp[auxMeio]) <= 0) {
-//                data[i] = temp[auxInicio++];
-//            } else {
-//                data[i] = temp[auxMeio++];
-//            }
-//        }
-//
-//        super.setInfo(data);
+
+        super.setInfo(data);
     }
     
     public static void main(String[] args) {
